d27bc9e8aaa09c26ef1b1eb22ba9be890fdf90cb
betarunex <betarunex@gmail.com>
2020-05-05 13:33:13 +0200

    Add interceptor to run before/after each request

---
+++ src/main/java/com/simple/testapp/AppConfig.java
@@ -0,0 +1,17 @@
+package com.simple.testapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Autowired
+    private RequestLoggingInterceptor requestLoggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry reg) {
+        reg.addInterceptor(requestLoggingInterceptor);
+    }
+}
--- src/main/java/com/simple/testapp/RequestLoggingInterceptor.java
+++ src/main/java/com/simple/testapp/RequestLoggingInterceptor.java
@@ -5,18 +5,23 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
 
 @Component
-public class RequestInterceptor extends HandlerInterceptorAdapter {
+public class RequestLoggingInterceptor extends HandlerInterceptorAdapter {
+
+    private static final String START_TIMER_ATTRIB = "startTimer";
 
     @Override
     public boolean preHandle(final HttpServletRequest req, final HttpServletResponse res, final Object handler) {
         // start timer
+        req.setAttribute(START_TIMER_ATTRIB, Instant.now().toEpochMilli());
         return true;
     }
 
     @Override
     public void afterCompletion(final HttpServletRequest req, final HttpServletResponse res, final Object handler, final Exception exception) {
         // get time diff
+        System.out.println("Timer testing " + req.getAttribute(START_TIMER_ATTRIB));
     }
 }
