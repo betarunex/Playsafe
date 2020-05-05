6a1d2d0d781000dd67df1d29d1bb9d7a232eb706
betarunex <betarunex@gmail.com>
2020-05-05 13:18:53 +0200

    Mapped endpoints

--- src/main/java/com/simple/testapp/ConversionsEndpoint.java
+++ src/main/java/com/simple/testapp/ConversionsEndpoint.java
@@ -1,21 +1,29 @@
 package com.simple.testapp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConversionsEndpoint {
     private static double KELVIN_FREEZE_TEMP = 273.15;
     private static double MILE_TO_KM = 1.60934;
 
+    @RequestMapping("/ktoc")
     public static double kelvinToCelcius(final double kelvin) {
         return kelvin - KELVIN_FREEZE_TEMP;
     }
 
+    @RequestMapping("/ctok")
     public static double celciusToKelvin(final double celcius) {
         return celcius + KELVIN_FREEZE_TEMP;
     }
 
+    @RequestMapping("/mtok")
     public static double milesToKilometers(final double miles) {
         return miles / MILE_TO_KM;
     }
 
+    @RequestMapping("/ktom")
     public static double kilometersToMiles(final double kilometers) {
         return kilometers * MILE_TO_KM;
     }
